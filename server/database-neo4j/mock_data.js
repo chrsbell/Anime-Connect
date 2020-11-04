64c8fc4e1315dd5d43e595c13a1ae20ddc266286
Chris Bell <chrisbell.dev@gmail.com>
2020-11-03 17:47:06 -0800

    Adjust schema, mock data

---
+++ server/database-neo4j/mock_data.js
@@ -0,0 +1,652 @@
+module.exports = [
+  {
+    node: {
+      id: 5114,
+      title: 'Fullmetal Alchemist: Brotherhood',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1223/96541.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1223/96541l.jpg',
+      },
+    },
+    ranking: {
+      rank: 1,
+    },
+  },
+  {
+    node: {
+      id: 9253,
+      title: 'Steins;Gate',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/5/73199.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/5/73199l.jpg',
+      },
+    },
+    ranking: {
+      rank: 2,
+    },
+  },
+  {
+    node: {
+      id: 28977,
+      title: 'Gintama°',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/3/72078.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/3/72078l.jpg',
+      },
+    },
+    ranking: {
+      rank: 3,
+    },
+  },
+  {
+    node: {
+      id: 11061,
+      title: 'Hunter x Hunter (2011)',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/11/33657.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/11/33657l.jpg',
+      },
+    },
+    ranking: {
+      rank: 4,
+    },
+  },
+  {
+    node: {
+      id: 820,
+      title: 'Ginga Eiyuu Densetsu',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/13/13225.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/13/13225l.jpg',
+      },
+    },
+    ranking: {
+      rank: 5,
+    },
+  },
+  {
+    node: {
+      id: 9969,
+      title: "Gintama'",
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/4/50361.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/4/50361l.jpg',
+      },
+    },
+    ranking: {
+      rank: 6,
+    },
+  },
+  {
+    node: {
+      id: 38524,
+      title: 'Shingeki no Kyojin Season 3 Part 2',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1517/100633.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1517/100633l.jpg',
+      },
+    },
+    ranking: {
+      rank: 7,
+    },
+  },
+  {
+    node: {
+      id: 15417,
+      title: "Gintama': Enchousen",
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/6/75172.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/6/75172l.jpg',
+      },
+    },
+    ranking: {
+      rank: 8,
+    },
+  },
+  {
+    node: {
+      id: 35180,
+      title: '3-gatsu no Lion 2nd Season',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/3/88469.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/3/88469l.jpg',
+      },
+    },
+    ranking: {
+      rank: 9,
+    },
+  },
+  {
+    node: {
+      id: 28851,
+      title: 'Koe no Katachi',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1122/96435.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1122/96435l.jpg',
+      },
+    },
+    ranking: {
+      rank: 10,
+    },
+  },
+  {
+    node: {
+      id: 32281,
+      title: 'Kimi no Na wa.',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/5/87048.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/5/87048l.jpg',
+      },
+    },
+    ranking: {
+      rank: 11,
+    },
+  },
+  {
+    node: {
+      id: 34096,
+      title: 'Gintama.',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/3/83528.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/3/83528l.jpg',
+      },
+    },
+    ranking: {
+      rank: 12,
+    },
+  },
+  {
+    node: {
+      id: 918,
+      title: 'Gintama',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/10/73274.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/10/73274l.jpg',
+      },
+    },
+    ranking: {
+      rank: 13,
+    },
+  },
+  {
+    node: {
+      id: 15335,
+      title: 'Gintama Movie 2: Kanketsu-hen - Yorozuya yo Eien Nare',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/10/51723.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/10/51723l.jpg',
+      },
+    },
+    ranking: {
+      rank: 14,
+    },
+  },
+  {
+    node: {
+      id: 4181,
+      title: 'Clannad: After Story',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/13/24647.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/13/24647l.jpg',
+      },
+    },
+    ranking: {
+      rank: 15,
+    },
+  },
+  {
+    node: {
+      id: 35247,
+      title: 'Owarimonogatari 2nd Season',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/6/87322.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/6/87322l.jpg',
+      },
+    },
+    ranking: {
+      rank: 16,
+    },
+  },
+  {
+    node: {
+      id: 2904,
+      title: 'Code Geass: Hangyaku no Lelouch R2',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/4/9391.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/4/9391l.jpg',
+      },
+    },
+    ranking: {
+      rank: 17,
+    },
+  },
+  {
+    node: {
+      id: 32935,
+      title: 'Haikyuu!!: Karasuno Koukou vs. Shiratorizawa Gakuen Koukou',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/7/81992.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/7/81992l.jpg',
+      },
+    },
+    ranking: {
+      rank: 18,
+    },
+  },
+  {
+    node: {
+      id: 37510,
+      title: 'Mob Psycho 100 II',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1918/96303.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1918/96303l.jpg',
+      },
+    },
+    ranking: {
+      rank: 19,
+    },
+  },
+  {
+    node: {
+      id: 37491,
+      title: 'Gintama.: Shirogane no Tamashii-hen - Kouhan-sen',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1776/96566.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1776/96566l.jpg',
+      },
+    },
+    ranking: {
+      rank: 20,
+    },
+  },
+  {
+    node: {
+      id: 199,
+      title: 'Sen to Chihiro no Kamikakushi',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/6/79597.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/6/79597l.jpg',
+      },
+    },
+    ranking: {
+      rank: 21,
+    },
+  },
+  {
+    node: {
+      id: 31758,
+      title: 'Kizumonogatari III: Reiketsu-hen',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/8/82911.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/8/82911l.jpg',
+      },
+    },
+    ranking: {
+      rank: 22,
+    },
+  },
+  {
+    node: {
+      id: 36838,
+      title: 'Gintama.: Shirogane no Tamashii-hen',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/12/89603.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/12/89603l.jpg',
+      },
+    },
+    ranking: {
+      rank: 23,
+    },
+  },
+  {
+    node: {
+      id: 40591,
+      title: 'Kaguya-sama wa Kokurasetai?: Tensai-tachi no Renai Zunousen',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1764/106659.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1764/106659l.jpg',
+      },
+    },
+    ranking: {
+      rank: 24,
+    },
+  },
+  {
+    node: {
+      id: 1,
+      title: 'Cowboy Bebop',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/4/19644.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/4/19644l.jpg',
+      },
+    },
+    ranking: {
+      rank: 25,
+    },
+  },
+  {
+    node: {
+      id: 36862,
+      title: 'Made in Abyss Movie 3: Fukaki Tamashii no Reimei',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1175/101926.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1175/101926l.jpg',
+      },
+    },
+    ranking: {
+      rank: 26,
+    },
+  },
+  {
+    node: {
+      id: 33095,
+      title: 'Shouwa Genroku Rakugo Shinjuu: Sukeroku Futatabi-hen',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/10/82947.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/10/82947l.jpg',
+      },
+    },
+    ranking: {
+      rank: 27,
+    },
+  },
+  {
+    node: {
+      id: 23273,
+      title: 'Shigatsu wa Kimi no Uso',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/3/67177.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/3/67177l.jpg',
+      },
+    },
+    ranking: {
+      rank: 28,
+    },
+  },
+  {
+    node: {
+      id: 17074,
+      title: 'Monogatari Series: Second Season',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/3/52133.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/3/52133l.jpg',
+      },
+    },
+    ranking: {
+      rank: 29,
+    },
+  },
+  {
+    node: {
+      id: 28891,
+      title: 'Haikyuu!! Second Season',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/9/76662.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/9/76662l.jpg',
+      },
+    },
+    ranking: {
+      rank: 30,
+    },
+  },
+  {
+    node: {
+      id: 34599,
+      title: 'Made in Abyss',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/6/86733.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/6/86733l.jpg',
+      },
+    },
+    ranking: {
+      rank: 31,
+    },
+  },
+  {
+    node: {
+      id: 24701,
+      title: 'Mushishi Zoku Shou 2nd Season',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/9/68095.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/9/68095l.jpg',
+      },
+    },
+    ranking: {
+      rank: 32,
+    },
+  },
+  {
+    node: {
+      id: 33050,
+      title: "Fate/stay night Movie: Heaven's Feel - III. Spring Song",
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1400/104685.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1400/104685l.jpg',
+      },
+    },
+    ranking: {
+      rank: 33,
+    },
+  },
+  {
+    node: {
+      id: 263,
+      title: 'Hajime no Ippo',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/4/86334.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/4/86334l.jpg',
+      },
+    },
+    ranking: {
+      rank: 34,
+    },
+  },
+  {
+    node: {
+      id: 40730,
+      title: 'Tian Guan Ci Fu',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1319/109301.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1319/109301l.jpg',
+      },
+    },
+    ranking: {
+      rank: 35,
+    },
+  },
+  {
+    node: {
+      id: 44,
+      title: 'Rurouni Kenshin: Meiji Kenkaku Romantan - Tsuioku-hen',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1807/102419.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1807/102419l.jpg',
+      },
+    },
+    ranking: {
+      rank: 36,
+    },
+  },
+  {
+    node: {
+      id: 164,
+      title: 'Mononoke Hime',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/7/75919.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/7/75919l.jpg',
+      },
+    },
+    ranking: {
+      rank: 37,
+    },
+  },
+  {
+    node: {
+      id: 19,
+      title: 'Monster',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/10/18793.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/10/18793l.jpg',
+      },
+    },
+    ranking: {
+      rank: 38,
+    },
+  },
+  {
+    node: {
+      id: 21939,
+      title: 'Mushishi Zoku Shou',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/13/58533.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/13/58533l.jpg',
+      },
+    },
+    ranking: {
+      rank: 39,
+    },
+  },
+  {
+    node: {
+      id: 1575,
+      title: 'Code Geass: Hangyaku no Lelouch',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/5/50331.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/5/50331l.jpg',
+      },
+    },
+    ranking: {
+      rank: 40,
+    },
+  },
+  {
+    node: {
+      id: 37521,
+      title: 'Vinland Saga',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1500/103005.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1500/103005l.jpg',
+      },
+    },
+    ranking: {
+      rank: 41,
+    },
+  },
+  {
+    node: {
+      id: 38329,
+      title: 'Seishun Buta Yarou wa Yumemiru Shoujo no Yume wo Minai',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1613/102179.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1613/102179l.jpg',
+      },
+    },
+    ranking: {
+      rank: 42,
+    },
+  },
+  {
+    node: {
+      id: 245,
+      title: 'Great Teacher Onizuka',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/13/11460.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/13/11460l.jpg',
+      },
+    },
+    ranking: {
+      rank: 43,
+    },
+  },
+  {
+    node: {
+      id: 457,
+      title: 'Mushishi',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/2/73862.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/2/73862l.jpg',
+      },
+    },
+    ranking: {
+      rank: 44,
+    },
+  },
+  {
+    node: {
+      id: 5258,
+      title: 'Hajime no Ippo: New Challenger',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/8/56617.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/8/56617l.jpg',
+      },
+    },
+    ranking: {
+      rank: 45,
+    },
+  },
+  {
+    node: {
+      id: 11665,
+      title: 'Natsume Yuujinchou Shi',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/3/37449.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/3/37449l.jpg',
+      },
+    },
+    ranking: {
+      rank: 46,
+    },
+  },
+  {
+    node: {
+      id: 431,
+      title: 'Howl no Ugoku Shiro',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/5/75810.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/5/75810l.jpg',
+      },
+    },
+    ranking: {
+      rank: 47,
+    },
+  },
+  {
+    node: {
+      id: 38000,
+      title: 'Kimetsu no Yaiba',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/1286/99889.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/1286/99889l.jpg',
+      },
+    },
+    ranking: {
+      rank: 48,
+    },
+  },
+  {
+    node: {
+      id: 7311,
+      title: 'Suzumiya Haruhi no Shoushitsu',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/2/73842.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/2/73842l.jpg',
+      },
+    },
+    ranking: {
+      rank: 49,
+    },
+  },
+  {
+    node: {
+      id: 2001,
+      title: 'Tengen Toppa Gurren Lagann',
+      main_picture: {
+        medium: 'https://api-cdn.myanimelist.net/images/anime/4/5123.jpg',
+        large: 'https://api-cdn.myanimelist.net/images/anime/4/5123l.jpg',
+      },
+    },
+    ranking: {
+      rank: 50,
+    },
+  },
+];
---
+++ server/database-neo4j/mock_data_test.cypher
@@ -0,0 +1,8 @@
+MERGE (u:User {name: 'del', joined_at: '2017-03-28T01:11:28+00:00', picture: 'https://cdn.myanimelist.net/images/userimages/393587.jpg?t=1601167200'})
+MERGE (u:User {name: 'tokufi', joined_at: '2017-03-28T01:11:28+00:00', picture: 'https://cdn.myanimelist.net/images/userimages/3973725.jpg?t=1604439000'})
+MERGE (u:User {name: 'diao', joined_at: '2017-03-28T01:11:28+00:00', picture: 'https://api-cdn.myanimelist.net/images/userimages/6135186.jpg?t=1604440200'})
+MERGE (a:Anime {title: 'Cowboy Bebop', rank: 25})
+// MATCH (u:User {name: 'diao'}) // find the user
+MERGE (u)-[:LIKES]->(a)
+MERGE (b:Anime {title : 'Steins;Gate', rank: 2})
+MERGE (u)-[:LIKES]->(b)
