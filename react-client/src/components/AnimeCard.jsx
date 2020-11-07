8465a1c0aa3c28fd57ff272ce1a242cfd4030c06
Chris Bell <chrisbell.dev@gmail.com>
2020-11-06 23:53:28 -0800

    Use DataTable for Anime List

--- react-client/src/components/AnimeCard.jsx
+++ react-client/src/components/AnimeCard.jsx
@@ -1,21 +1,10 @@
 import React, { useState } from 'react';
-import {
-  Avatar,
-  Box,
-  Card,
-  Heading,
-  CardBody,
-  CardHeader,
-  Grid,
-  Grommet,
-  Text,
-  Image,
-  Stack,
-} from 'grommet';
+import { Text, Image, Layer, Box } from 'grommet';
 import styled from 'styled-components';
+import AnimeEntryModal from './AnimeEntryModal.jsx';
 
-const StyledCard = styled(Card).attrs((props) => {
-  return { className: props.animation };
+const StyledImage = styled(Image).attrs((props) => {
+  return { className: props.hoverClass };
 })`
   @keyframes grow {
     from {
@@ -42,38 +31,27 @@ const StyledCard = styled(Card).attrs((props) => {
 `;
 
 const AnimeCard = ({ entry }) => {
-  // set the animation state using hooks
-  const [animation, setAnimation] = useState('none');
+  const [hoverClass, setHoverClass] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <StyledCard
-      elevation="large"
-      onMouseOver={() => {
-        setAnimation('grow');
-      }}
-      onMouseLeave={() => {
-        setAnimation('shrink');
-      }}
-      width="300px"
-      height="450px"
-      animation={animation}
-    >
-      <CardBody height="medium">
-        <Image fit="cover" src={entry.node.main_picture.large} />
-      </CardBody>
-      <CardHeader
-        pad={{ horizontal: 'small', vertical: 'small' }}
-        background="#000000A0"
-        width="medium"
-        justify="start"
-      >
-        <Box>
-          <Text size="small" margin="none">
-            {entry.node.title}
-          </Text>
-        </Box>
-      </CardHeader>
-    </StyledCard>
+    <>
+      {showModal ? <AnimeEntryModal entry={entry} /> : null}
+      <StyledImage
+        onMouseOver={() => {
+          setHoverClass('grow');
+        }}
+        onMouseLeave={() => {
+          setHoverClass('shrink');
+        }}
+        onClick={() => setShowModal(true)}
+        hoverClass={hoverClass}
+        fit="contain"
+        width="small"
+        round="false"
+        src={entry.main_picture.medium}
+      />
+    </>
   );
 };
 
--- react-client/src/components/AnimeList.jsx
+++ react-client/src/components/AnimeList.jsx
@@ -1,17 +1,72 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 import AnimeCard from './AnimeCard.jsx';
-import { Box, Grid, ResponsiveContext, InfiniteScroll } from 'grommet';
+import { Box, Text, Heading, Image, Grid, ResponsiveContext, DataTable } from 'grommet';
+import styled from 'styled-components';
+import _ from 'underscore';
 
-const AnimeList = ({ list }) => {
+const AnimeList = () => {
   const size = useContext(ResponsiveContext);
+  let [animeList, setAnimeList] = useState([]);
+
+  // component mount
+  useEffect(() => {
+    return getTopAnime();
+  }, []);
+
+  // gets a list of top rated anime
+  const getTopAnime = () => {
+    let source = axios.CancelToken.source();
+    axios
+      .get('/api/anime/ranking', { cancelToken: source.token })
+      .then((res) => {
+        if (res.data.length) {
+          setAnimeList(
+            _.map(res.data, (info) => {
+              return _.extend({}, info.node, info.ranking);
+            })
+          );
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    return () => source.cancel('Cancelled /api/anime/ranking request during component unmount.');
+  };
+
+  // table columns
+  const columns = [
+    {
+      property: 'main_picture',
+      header: null,
+      align: 'start',
+      primary: true,
+      render: (datum) => <AnimeCard entry={datum} />,
+    },
+    {
+      property: 'title',
+      header: <Heading size="medium">Title</Heading>,
+    },
+    {
+      property: 'rank',
+      header: <Heading size="medium">Rank</Heading>,
+      align: 'end',
+    },
+  ];
+
   return (
-    <Box pad="large">
-      <Grid columns={size !== 'small' ? 'medium' : '100%'} gap="medium">
-        <InfiniteScroll items={list}>
-          {(item) => <AnimeCard key={item.node.id} entry={item} />}
-        </InfiniteScroll>
-      </Grid>
+    <Box align="center" pad="medium">
+      <DataTable
+        onClickRow={() => null}
+        border={{
+          color: 'border',
+          side: 'horizontal',
+          size: '1px',
+        }}
+        step={10}
+        columns={columns}
+        data={animeList}
+      />
     </Box>
   );
 };
